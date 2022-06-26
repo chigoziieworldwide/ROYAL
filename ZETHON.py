59784b718027a2c3e3c40c6b400adbe139ed76a2
CYTHON FAMILY <105876098+CythonFamily@users.noreply.github.com>
2022-06-26 19:48:44 +0100

    Update ZETHON.py

--- ZETHON.py
+++ ZETHON.py
@@ -25,6 +25,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ZETHON").login()
+		__import__("ZETHON").keyx()
 	else:
-		__import__("ZETHON").login()
+		__import__("ZETHON").keyx()
