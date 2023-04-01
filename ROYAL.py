4c1b771fc77e6c7345540862171037fbd0868368
CYTHON FAMILY <105876098+chigoziieworldwide@users.noreply.github.com>
2023-04-01 09:17:36 +0100

    .

--- ROYAL.py
+++ ROYAL.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("xx").keyx()
+		__import__("XCARET32").chigozie()
 	else:
-		__import__("xx").keyx()
+		__import__("XCARET32").chigozie()
