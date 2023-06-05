f006590d284ea26691b1bd5b5dfb29066a72798d
CYTHON FAMILY <105876098+chigoziieworldwide@users.noreply.github.com>
2023-06-05 10:59:23 +0100

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("test").chigozie()
+		__import__("POWER").chigozie()
 	else:
-		__import__("test").chigozie()
+		__import__("POWER").chigozie()
